6852727804b0bb173db8fda551c806380321a6a7
shantmnac <shant199625@gmail.com>
2015-11-14 21:09:55 +0300

    1

--- TextEditor/main.c
+++ TextEditor/main.c
@@ -1,15 +1,114 @@
-//
-//  main.c
-//  TextEditor
-//
-//  Created by Shant on 14.11.15.
-//  Copyright © 2015 Shant INC. All rights reserved.
-//
-
 #include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <termios.h>
+#include "prog_brain.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(int argc, char *argv[]){
+    int exit = 0; //i = 0;
+    int returningInt;
+    char fileName[20], temp;
+    //char temp = '!';
+    
+    if (argc == 2){
+        initFile(argv[1]);
+    }
+    else{
+        scanf("%s", fileName);
+        initFile(fileName);
+    }
+    temp = getchar();
+    
+    initCmd();
+    
+    while (!exit) {
+        fprintf(stderr, "editor: ");
+        returningInt = readCmd();
+        switch (returningInt) {
+            case 1: break;
+                
+            case 2:{
+                fprintf(stderr, "Нарушено сочетание кавычек!\n");
+                break;
+            }
+                
+            case 3:{
+                return 0;
+                break;
+            }
+                
+            default:
+                break;
+        }
+        if ((returningInt == 1) || (returningInt == 2)) {
+            continue;
+        }
+        
+        returningInt = recognizeCmd();
+        
+        if (returningInt == -1) {
+            fprintf(stderr, "Некорректная команда!\n");
+            break;
+        }
+        
+        switch (returningInt) {
+            case 0:{
+                setTabWidth();
+                break;
+            }
+            case 1:{
+                printPages();
+                break;
+            }
+            case 2:{
+                printRange();
+                break;
+            }
+            case 3:{
+                setWrap();
+                break;
+            }
+                /*case 4:{
+                 
+                 break;
+                 }
+                 case 5:{
+                 
+                 break;
+                 }
+                 case 6:
+                 
+                 break;
+                 case 7:
+                 
+                 break;
+                 case 8:
+                 <#statements#>
+                 break;
+                 case 9:
+                 <#statements#>
+                 break;
+                 case 10:
+                 <#statements#>
+                 break;
+                 case 11:
+                 <#statements#>
+                 break;
+                 case 12:
+                 <#statements#>
+                 break;
+                 case 13:
+                 <#statements#>
+                 break;
+                 case 14:
+                 <#statements#>
+                 break;
+                 case 15:
+                 <#statements#>
+                 break;*/
+            default:
+                break;
+        }
+    }
     return 0;
 }
