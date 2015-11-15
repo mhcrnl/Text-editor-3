842dc3dea4d2f9180af447c6e95d7cd45659d93f
shantmnac <shant199625@gmail.com>
2015-11-15 22:32:04 +0300

    e

---
+++ TextEditor/struct.h
@@ -0,0 +1,16 @@
+#ifndef struct_h
+#define struct_h
+
+struct listOfChars{
+    char curChar;
+    struct listOfChars *next;
+    struct listOfChars *prev;
+};
+
+struct listOfStrings{
+    struct listOfChars *curString;
+    struct listOfStrings *next;
+    struct listOfStrings *prev;
+};
+
+#endif
